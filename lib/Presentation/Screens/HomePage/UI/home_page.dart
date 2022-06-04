532170efa3ba9eb272a84c38d0aa3e7928eeff1e
vijayinyoutube <vijaythevirtue@gmail.com>
2022-06-04 22:15:29 +0530

    refactor

--- lib/Presentation/Screens/HomePage/UI/home_page.dart
+++ lib/Presentation/Screens/HomePage/UI/home_page.dart
@@ -1,5 +1,6 @@
+import '../../../../Data/Models/home_page_model.dart';
+
 import '../../../../Data/Repository/home_page_repo.dart';
-import '../../../Components/future_builder.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -12,6 +13,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late Future<DemoModel> futureDemoModel;
+
+  @override
+  void initState() {
+    futureDemoModel = DemoRepo().fetchData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,8 +28,28 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: FutureBuilderWidget(futureCall: DemoRepo().fetchData()),
+        child: FutureBuilder(
+          future: futureDemoModel,
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasData) {
+              return buildDataLayout(snapshot.data as DemoModel);
+            } else if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Waiting....');
+            } else if (snapshot.connectionState == ConnectionState.none) {
+              return const Text('Cannot establish connection with the server.');
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
+
+  Widget buildDataLayout(DemoModel data) => ListTile(
+        title: Text(data.userId.toString()),
+        subtitle: Text(data.title),
+        trailing: Text(data.completed.toString()),
+      );
 }
